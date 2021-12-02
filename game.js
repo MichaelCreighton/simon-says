a9093d2812d57d4845ae3a6dd7fb4f703afd19d9
Michael <mikl@mymts.net>
2021-12-02 08:33:07 -0600

    changed main-content to grid

--- game.js
+++ game.js
@@ -1,6 +1,4 @@
-
 // Javascript and JQuery demo
-
 
 const buttonColours = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
@@ -9,11 +7,11 @@ let level = 0;
 let started = false;
 
 // Watch for key press to start game
-$(document).keydown(function() {
-  if(!started) {
-      nextSequence();
-      started = true;
-  };
+$(document).keydown(function () {
+  if (!started) {
+    nextSequence();
+    started = true;
+  }
 });
 
 // add random button to game pattern
@@ -30,16 +28,16 @@ function nextSequence() {
   level += 1;
   $("#level-title").text("Level: " + level);
   userClickedPattern = [];
-};
+}
 
 // watch for button clicks and push the choice
 // to the users pattern
-$(".btn").click(function() {
-  if(started) {
+$(".btn").click(function () {
+  if (started) {
     userClickedPattern.push(this.id);
     animatePress(this.id);
     playSound(this.id);
-    
+
     checkAnswer(userClickedPattern.length - 1);
   }
 });
@@ -50,14 +48,15 @@ function playSound(name) {
   audio.play();
 }
 
-// animate the clicked button 
-
+// animate the clicked button
 
 function animatePress(currentColour) {
-  $("#" + currentColour).fadeOut(100).fadeIn(100);
+  $("#" + currentColour)
+    .fadeOut(100)
+    .fadeIn(100);
   $("#" + currentColour).addClass("pressed");
-  setTimeout(function() {
-  $("#" + currentColour).removeClass("pressed")
+  setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
   }, 100);
 }
 
@@ -65,19 +64,19 @@ function animatePress(currentColour) {
 // if correct add to game pattern, else
 // display final level reached and restart option
 function checkAnswer(currentLevel) {
-  if(userClickedPattern[currentLevel] !== gamePattern[currentLevel]) {
+  if (userClickedPattern[currentLevel] !== gamePattern[currentLevel]) {
     playSound("wrong");
     $("body").addClass("game-over");
-    setTimeout(function() {
+    setTimeout(function () {
       $("body").removeClass("game-over");
     }, 200);
     $("#level-title").text("GAME OVER!");
-    $(".score").text("Final Level: " + (level -1));
+    $(".score").text("Final Level: " + (level - 1));
     $(".press").text("Press any key...");
     startOver();
   }
-  if((currentLevel + 1) === level) {
-    setTimeout(function() {
+  if (currentLevel + 1 === level) {
+    setTimeout(function () {
       nextSequence();
     }, 1000);
   }
@@ -89,5 +88,3 @@ function startOver() {
   level = 0;
   started = false;
 }
-
-
